873697bff0b28a7157efd563518c1773b15b23d2
Jason Kuhrt <jasonkuhrt@me.com>
2015-03-31 17:31:20 -0400

    Update example into something that can execute the popover. DDD: Demo Driven Design.

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -4,14 +4,40 @@ import Popover from '../../lib'
 
 
 
+let tracer = React.createClass({
+  name: 'tracer',
+  getInitialState: function() {
+    return { popove: null }
+  },
+  openPopover() {
+    this.setState({
+      popover: Popover({
+        lockPoint: React.findDOMNode(this).firstElementChild
+      })
+    })
+  },
+  render() {
+    let popover = this.state.popover
+
+    let lockPoint = e.div({
+      style: lockPointStyle,
+      onClick: this.openPopover
+    }, '!')
+
+    return e.div({}, lockPoint, popover)
+  }
+})
+
+
+
 let app = React.createClass({
   name: 'basic-demo',
   render() {
-    let lockPoint = e.div({ style: lockPointStyle }, '!')
-    //let element = Popover({ lockPoint })
-    return e.div({}, lockPoint)
+    return React.createElement(tracer)
   }
 })
+
+
 
 let lockPointStyle = {
   width: 50,
