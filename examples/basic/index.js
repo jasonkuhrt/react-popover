af2c745162f494b91ccfcd4ef28ceddc5e79aa80
Jason Kuhrt <jasonkuhrt@me.com>
2015-06-11 11:29:05 -0400

    Feature: Allow users to specify a placement preference. Closes #1 and closes #24.

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -1,10 +1,14 @@
-import styles from './index.css'
-import React, { DOM as e } from 'react'
-import Popover from '../../lib'
+import Debug from 'debug'
+import R from 'ramda'
 import Draggable from 'react-draggable'
+import styles from './index.css'
+import React, { DOM as E } from 'react'
+import Popover from '../../lib'
+import * as Layout from '../../lib/layout'
 
 
 
+let debug = Debug('demo')
 Popover = React.createFactory(Popover)
 Draggable = React.createFactory(Draggable)
 
@@ -14,7 +18,8 @@ let Demo = React.createClass({
   name: 'demo',
   getInitialState: function() {
     return {
-      popoverIsOpen: false
+      popoverIsOpen: false,
+      preferPlace: null
     }
   },
   togglePopover(toState) {
@@ -23,7 +28,13 @@ let Demo = React.createClass({
       popoverIsOpen: toState
     })
   },
+  changePreferPlace(event) {
+    let preferPlace = event.target.value === 'null' ? null : event.target.value
+    this.setState({ preferPlace })
+  },
   render() {
+    debug('render')
+
     let targetProps = {
       className: ['Target', `is-${['open', 'closed'][Number(this.state.popoverIsOpen)]}`].join(' '),
       onDoubleClick: this.togglePopover
@@ -35,7 +46,7 @@ let Demo = React.createClass({
 
     let target = (
       Draggable(draggableProps,
-        e.div(targetProps,
+        E.div(targetProps,
           'Drag \nOR\nDouble\nClick'
         )
       )
@@ -43,18 +54,43 @@ let Demo = React.createClass({
 
     let popoverProps = {
       isOpen: this.state.popoverIsOpen,
+      preferPlace: this.state.preferPlace,
       onOuterAction: this.togglePopover.bind(null, false),
       body: [
-        e.h1({}, 'Popover Title'),
-        e.div({}, 'Popover contents.')
+        E.h1({}, 'Popover Title'),
+        E.div({}, 'Popover contents.')
       ]
     }
 
+    let controls = (
+      E.form({},
+        E.label({ htmlFor: 'preferPlace' }, 'preferPlace '),
+        E.select({ id: 'preferPlace', onChange: this.changePreferPlace },
+          createPreferPlaceOptions(Layout)
+        )
+      )
+    )
+
     let popover = Popover(popoverProps, target)
 
-    return popover
+    let app = E.div({}, controls, E.br(), popover)
+
+    return app
   }
 })
+
+
+let createOption = (type) => {
+  return E.option({ key: type, value: type }, type)
+}
+
+let createPreferPlaceOptions = R.compose(
+  R.prepend([E.option({ key: 'null', value: null }, 'null')]),
+  R.map(createOption),
+  R.flatten,
+  R.map(R.path(['values'])),
+  R.path(['types'])
+)
 
 
 
