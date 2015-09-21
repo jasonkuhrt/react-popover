4624f98e6576bf31d5b2da611032813c84727ecf
Jason Kuhrt <jasonkuhrt@me.com>
2015-09-21 08:20:03 -0400

    Linter

--- webpack.examples.config.babel.js
+++ webpack.examples.config.babel.js
@@ -3,28 +3,28 @@ import Path from 'path'
 
 
 
-let path = Path.join.bind(null, __dirname)
-let outputDir = path('build')
-let indexEntry = [ './examples/basic/index.js', 'webpack/hot/dev-server' ]
-let jsLoaders = [ 'react-hot', 'babel' ]
+const path = Path.join.bind(null, __dirname)
+const outputDir = path(`build`)
+const indexEntry = [ `./examples/basic/index.js`, `webpack/hot/dev-server` ]
+const jsLoaders = [ `react-hot`, `babel` ]
 
 export default {
   entry: {
-    index: indexEntry
+    index: indexEntry,
   },
   output: {
     path: outputDir,
-    filename: '[name].js'
+    filename: `[name].js`,
   },
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loaders: jsLoaders },
-      { test: /\.css$/, exclude: /node_modules/, loaders: [ 'style', 'css' ]}
-    ]
+      { test: /\.css$/, exclude: /node_modules/, loaders: [ `style`, `css` ]},
+    ],
   },
-  devtool: 'source-map',
+  devtool: `source-map`,
   devServer: {
-    contentBase: outputDir
+    contentBase: outputDir,
   },
-  plugins: [Bootstrap({})]
+  plugins: [Bootstrap({})],
 }
--- webpack.test.config.babel.js
+++ webpack.test.config.babel.js
@@ -2,27 +2,26 @@ import Path from 'path'
 
 
 
-let path = Path.join.bind(null, __dirname)
-
-let outputDir = path('./build')
-let indexEntry = ['./test/index.coffee']
+const path = Path.join.bind(null, __dirname)
+const outputDir = path(`./build`)
+const indexEntry = [`./test/index.coffee`]
 
 export default {
   entry: {
-    index: indexEntry
+    index: indexEntry,
   },
   output: {
     path: outputDir,
-    filename: '[name].js'
+    filename: `[name].js`,
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      { test: /\.coffee$/, exclude: /node_modules/, loaders: ['coffee']}
-    ]
+      { test: /\.js$/, exclude: /node_modules/, loaders: [`babel`]},
+      { test: /\.coffee$/, exclude: /node_modules/, loaders: [`coffee`]},
+    ],
   },
-  devtool: 'source-map',
+  devtool: `source-map`,
   devServer: {
-    contentBase: outputDir
-  }
+    contentBase: outputDir,
+  },
 }
