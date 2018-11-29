48cfe158f5ca7cf9a02043b3d61f028ee9c42c55
Jason Kuhrt <jasonkuhrt@me.com>
2018-11-28 22:56:47 -0500

    chore: minimal storybook layout

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,14 @@
 import { configure } from "@storybook/react"
+import { setOptions } from "@storybook/addon-options"
 
 function loadStories() {
   require("../stories")
 }
+
+setOptions({
+  showAddonPanel: false,
+  showSearchBox: false,
+  showStoriesPanel: false,
+})
 
 configure(loadStories, module)
